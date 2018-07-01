015a41c5a8e22505df1c5f359fd9a379d15a2703
Julian Jark <juliuxu@gmail.com>
2018-07-01 16:35:36 +0200

    MapView with current location

--- omen/App.js
+++ omen/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import Map from "./Map";
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello, World!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Map />
       </View>
     );
   }
@@ -16,8 +16,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: "#fff"
+    // alignItems: "center",
+    // justifyContent: "center"
   }
 });
