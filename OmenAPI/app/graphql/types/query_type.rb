ecf8fdd7c427f4952db578e133ed1beb7cec6e7d
Victor Rudolfsson <>
2018-07-01 18:01:34 +0200

    Fixed location search

--- OmenAPI/app/graphql/types/query_type.rb
+++ OmenAPI/app/graphql/types/query_type.rb
@@ -3,12 +3,18 @@ class Types::QueryType < Types::BaseObject
   # They will be entry points for queries on your schema.
 
   # TODO: remove me
-  field :omen, Types::OmenType.connection_type, null: false, description: 'Fetch Omen' do
+  field :omen, Types::OmenType.connection_type, null: false, description: 'Fetch Omen'
+
+  def omen(latitude: nil, longitude: nil)
+    Oman.all
+  end
+
+  field :nearby, Types::OmenType.connection_type, null: false, description: 'Fetch Omen' do
     argument :latitude, Float, required: true
     argument :longitude, Float, required: true
   end
 
-  def omen(latitude: nil, longitude: nil)
-    Oman.within(5, :units => :kms, origin: [latitude, longitude]).by_distance(origin: [latitude, longitude])
+  def nearby(latitude: nil, longitude: nil)
+    Oman.joins(:location).by_distance(origin: [latitude, longitude])
   end
 end
